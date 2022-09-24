8aa2404812974bfabbf17f28859f8653ce17f3f5
Levi Oliveira <levi.carvalho99@gmail.com>
2022-09-23 21:35:03 -0300

    feat: inicializando as controllers de user, chat e message com metodos basicos de um crud

---
+++ src/microServices/api/controller/Chats.js
@@ -0,0 +1,7 @@
+exports.create = async (req, res) => {};
+
+exports.remove = async (req, res) => {};
+
+exports.update = async (req, res) => {};
+
+exports.show = async (req, res) => {};
---
+++ src/microServices/api/controller/Users.js
@@ -0,0 +1,7 @@
+module.exports.create = async (req, res) => {};
+
+module.exports.remove = async (req, res) => {};
+
+module.exports.update = async (req, res) => {};
+
+module.exports.show = async (req, res) => {};
